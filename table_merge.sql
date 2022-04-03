fd2af52d2d46405b5c0cc9ec2c740f8a55018607
alexyyyyytytytytyt <95461634+alexyyyyytytytytyt@users.noreply.github.com>
2022-04-02 18:43:48 -0700

    paimon

---
+++ table_merge.sql
@@ -0,0 +1,30 @@
+drop table oscar_filter;
+
+create table oscar_filter (
+	numbest int,
+	year_film int,
+	category varchar,
+	film varchar,
+	winner boolean
+);
+
+select * from oscar_filter;
+
+drop table omdb_api;
+
+create table omdb_api(
+	numbest int,
+	title varchar,
+	boxoffice varchar,
+	imdbrating decimal,
+	metascore int
+)
+
+select * from omdb_api
+
+select oscar_filter.film, omdb_api.boxoffice, omdb_api.imdbrating, omdb_api.metascore, oscar_filter.year_film, oscar_filter.category, oscar_filter.winner
+from oscar_filter
+inner join omdb_api on
+oscar_filter.film = omdb_api.title
+
+
